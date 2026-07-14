e4752f802838a68112931779755cc0883de2c2e2
Chunheng Zhao <39581025+Maccchiatooo@users.noreply.github.com>
2026-07-13 21:57:36 -0700

    Update lbm.hpp

--- 3D MPI+Kokkos+MPIIO/lbm.hpp
+++ 3D MPI+Kokkos+MPIIO/lbm.hpp
@@ -21,94 +21,31 @@
 #define dim 3
 #define ghost 3
 
+typedef Kokkos::TeamPolicy<> team_policy;
+typedef Kokkos::TeamPolicy<>::member_type member_type;
 typedef Kokkos::RangePolicy<> range_policy;
 typedef Kokkos::MDRangePolicy<Kokkos::Rank<2>> mdrange_policy2;
 typedef Kokkos::MDRangePolicy<Kokkos::Rank<3>> mdrange_policy3;
 typedef Kokkos::MDRangePolicy<Kokkos::Rank<4>> mdrange_policy4;
 
-using buffer_t = Kokkos::View<double ***, Kokkos::LayoutLeft, Kokkos::CudaUVMSpace>;
-using buffer_ut = Kokkos::View<double **, Kokkos::LayoutLeft, Kokkos::CudaUVMSpace>;
-using buffer_st = Kokkos::View<double *, Kokkos::LayoutLeft, Kokkos::CudaUVMSpace>;
+using buffer_f = Kokkos::View<double ****, Kokkos::CudaSpace>;
+using buffer_u = Kokkos::View<double ***, Kokkos::CudaSpace>;
+using buffer_div = Kokkos::View<double ***, Kokkos::CudaSpace>;
+using buffer_pack_f = Kokkos::View<double ****, Kokkos::CudaSpace>;
+using buffer_pack_u = Kokkos::View<double ***, Kokkos::CudaSpace>;
 
-struct CommHelper
+
+struct LBM
 {
 
     MPI_Comm comm;
-    // rank number for each dim
+    int nranks;
+
     int rx, ry, rz;
     // rank
     int me;
     // axis for each rank
     int px, py, pz;
-
-    // 6 faces
-    int up, down, left, right, front, back;
-    // 12 edges
-    int frontup, frontdown, frontleft, frontright, backup, backdown, backleft, backright, leftup, leftdown, rightup, rightdown;
-    // 8 points
-    int frontleftup, frontleftdown, frontrightup, frontrightdown, backleftup, backrightup, backleftdown, backrightdown;
-
-    CommHelper(MPI_Comm comm_)
-    {
-        comm = comm_;
-        int nranks;
-        MPI_Comm_size(comm, &nranks);
-        MPI_Comm_rank(comm, &me);
-
-        rx = std::pow(1.0 * nranks, 1.0 / 3.0);
-        while (nranks % rx != 0)
-            rx++;
-
-        rz = std::sqrt(1.0 * (nranks / rx));
-        while ((nranks / rx) % rz != 0)
-            rz++;
-
-        ry = nranks / rx / rz;
-
-        px = me % rx;
-        pz = (me / rx) % rz;
-        py = (me / rx / rz);
-        // 6 faces
-        left = px == 0 ? -1 : me - 1;
-        right = px == rx - 1 ? -1 : me + 1;
-        down = pz == 0 ? -1 : me - rx;
-        up = pz == rz - 1 ? -1 : me + rx;
-        front = py == 0 ? -1 : me - rx * rz;
-        back = py == ry - 1 ? -1 : me + rx * rz;
-        // 12 edges
-        leftup = (px == 0 || pz == rz - 1) ? -1 : me - 1 + rx;
-        rightup = (px == rx - 1 || pz == rz - 1) ? -1 : me + 1 + rx;
-        leftdown = (px == 0 || pz == 0) ? -1 : me - 1 - rx;
-        rightdown = (px == rx - 1 || pz == 0) ? -1 : me + 1 - rx;
-        frontup = (py == 0 || pz == rz - 1) ? -1 : me - rx * rz + rx;
-        frontdown = (py == 0 || pz == 0) ? -1 : me - rx * rz - rx;
-        frontleft = (py == 0 || px == 0) ? -1 : me - rx * rz - 1;
-        frontright = (py == 0 || px == rx - 1) ? -1 : me - rx * rz + 1;
-        backup = (py == ry - 1 || pz == rz - 1) ? -1 : me + rx * rz + rx;
-        backdown = (py == ry - 1 || pz == 0) ? -1 : me + rx * rz - rx;
-        backleft = (py == ry - 1 || px == 0) ? -1 : me + rx * rz - 1;
-        backright = (py == ry - 1 || px == rx - 1) ? -1 : me + rx * rz + 1;
-        // 8 points
-        frontleftdown = (py == 0 || px == 0 || pz == 0) ? -1 : me - rx * rz - rx - 1;
-        frontrightdown = (py == 0 || px == rx - 1 || pz == 0) ? -1 : me - rx * rz - rx + 1;
-        frontrightup = (py == 0 || px == rx - 1 || pz == rz - 1) ? -1 : me - rx * rz + rx + 1;
-        frontleftup = (py == 0 || px == 0 || pz == rz - 1) ? -1 : me - rx * rz + rx - 1;
-        backleftdown = (py == ry - 1 || px == 0 || pz == 0) ? -1 : me + rx * rz - rx - 1;
-        backrightdown = (py == ry - 1 || px == rx - 1 || pz == 0) ? -1 : me + rx * rz - rx + 1;
-        backrightup = (py == ry - 1 || px == rx - 1 || pz == rz - 1) ? -1 : me + rx * rz + rx + 1;
-        backleftup = (py == ry - 1 || px == 0 || pz == rz - 1) ? -1 : me + rx * rz + rx - 1;
-        // output the direction;
-        MPI_Barrier(MPI_COMM_WORLD);
-    }
-};
-
-struct LBM
-{
-
-    CommHelper comm;
-    MPI_Request mpi_requests_recv[26];
-    MPI_Request mpi_requests_send[26];
-    int mpi_active_requests;
 
     int glx, gly, glz;
     // include ghost nodes
@@ -119,73 +56,71 @@ struct LBM
     // local axis
     int x_lo = 0, x_hi = 0, y_lo = 0, y_hi = 0, z_lo = 0, z_hi = 0;
     double rho0, mu, cs2, tau0, u0;
-
-    // 6 faces
-    double *m_left, *m_right, *m_down, *m_up, *m_front, *m_back;
-    double *m_leftout, *m_rightout, *m_downout, *m_upout, *m_frontout, *m_backout;
+    int face_recv[6], face_send[6];
     // 12 edges
-    double *m_leftup, *m_rightup, *m_leftdown, *m_rightdown, *m_frontup, *m_backup, *m_frontdown, *m_backdown, *m_frontleft, *m_backleft, *m_frontright, *m_backright;
-    double *m_leftupout, *m_rightupout, *m_leftdownout, *m_rightdownout, *m_frontupout, *m_backupout, *m_frontdownout, *m_backdownout, *m_frontleftout, *m_backleftout, *m_frontrightout, *m_backrightout;
+    int edge_recv[12], edge_send[12];
     // 8 points
-    double *m_frontleftup, *m_frontrightup, *m_frontleftdown, *m_frontrightdown, *m_backleftup, *m_backleftdown, *m_backrightup, *m_backrightdown;
-    double *m_frontleftupout, *m_frontrightupout, *m_frontleftdownout, *m_frontrightdownout, *m_backleftupout, *m_backleftdownout, *m_backrightupout, *m_backrightdownout;
+    int point_recv[8], point_send[8];
+
+    buffer_pack_f f_face_send[6], f_face_recv[6];
+    buffer_pack_f f_edge_send[12], f_edge_recv[12];
+    buffer_pack_f f_corner_send[8], f_corner_recv[8];
+
+    buffer_pack_f m_left, m_right, m_down, m_up, m_front, m_back;
+    buffer_pack_f m_leftout, m_rightout, m_downout, m_upout, m_frontout, m_backout;
+    buffer_pack_f m_leftup, m_rightup, m_leftdown, m_rightdown, m_frontup, m_backup, m_frontdown, m_backdown, m_frontleft, m_backleft, m_frontright, m_backright;
+    buffer_pack_f m_leftupout, m_rightupout, m_leftdownout, m_rightdownout, m_frontupout, m_backupout, m_frontdownout, m_backdownout, m_frontleftout, m_backleftout, m_frontrightout, m_backrightout;
+    // 8 points
+    buffer_pack_f m_frontleftup, m_frontrightup, m_frontleftdown, m_frontrightdown, m_backleftup, m_backleftdown, m_backrightup, m_backrightdown;
+    buffer_pack_f m_frontleftupout, m_frontrightupout, m_frontleftdownout, m_frontrightdownout, m_backleftupout, m_backleftdownout, m_backrightupout, m_backrightdownout;
+
+    buffer_pack_u u_left, u_right, u_down, u_up, u_front, u_back;
+    buffer_pack_u u_leftout, u_rightout, u_downout, u_upout, u_frontout, u_backout;
+
+    buffer_pack_u u_leftup, u_rightup, u_leftdown, u_rightdown, u_backleft, u_backright, u_frontleft, u_frontright, u_backdown, u_backup, u_frontdown, u_frontup;
+    buffer_pack_u u_leftupout, u_rightupout, u_leftdownout, u_rightdownout, u_backleftout, u_backrightout, u_frontleftout, u_frontrightout, u_backdownout, u_backupout, u_frontdownout, u_frontupout;
+
+    buffer_pack_u u_frontleftdown, u_frontrightdown, u_frontleftup, u_frontrightup, u_backleftdown, u_backrightdown, u_backleftup, u_backrightup;
+    buffer_pack_u u_frontleftdownout, u_frontrightdownout, u_frontleftupout, u_frontrightupout, u_backleftdownout, u_backrightdownout, u_backleftupout, u_backrightupout;
+
     // particle distribution eqution
-    Kokkos::View<double ****, Kokkos::CudaUVMSpace> f, ft, fb;
+    buffer_f f, ft, fb;
     // macro scopic equation
-    Kokkos::View<double ***, Kokkos::CudaUVMSpace> ua, va, wa, rho, p;
+    buffer_u ua, va, wa, rho, p;
     // usr define
-    Kokkos::View<int ***, Kokkos::CudaUVMSpace> usr, ran;
+    Kokkos::View<int ***, Kokkos::CudaSpace> usr, ran;
     // bounce back notation
-    Kokkos::View<int *, Kokkos::CudaUVMSpace> bb;
+    Kokkos::View<int *, Kokkos::CudaSpace> bb;
     // weight function
-    Kokkos::View<double *, Kokkos::CudaUVMSpace> t;
+    Kokkos::View<double *, Kokkos::CudaSpace> t;
     // discrete velocity
-    Kokkos::View<int **, Kokkos::CudaUVMSpace> e;
+    Kokkos::View<int **, Kokkos::CudaSpace> e;
 
-    LBM(MPI_Comm comm_, int sx, int sy, int sz, double &tau, double &rho0, double &u0) : comm(comm_), glx(sx), gly(sy), glz(sz), tau0(tau), rho0(rho0), u0(u0)
+    LBM(MPI_Comm comm_, int sx, int sy, int sz, double &tau, double &rho0, double &u0)
+        : comm(comm_), glx(sx), gly(sy), glz(sz), tau0(tau), rho0(rho0), u0(u0)
     {
-        // local length
-        l_l[0] = (comm.px - glx % comm.rx >= 0) ? glx / comm.rx : glx / comm.rx + 1;
-        l_l[1] = (comm.py - gly % comm.ry >= 0) ? gly / comm.ry : gly / comm.ry + 1;
-        l_l[2] = (comm.pz - glz % comm.rz >= 0) ? glz / comm.rz : glz / comm.rz + 1;
-        // local length
-        lx = l_l[0] + 2 * ghost;
-        ly = l_l[1] + 2 * ghost;
-        lz = l_l[2] + 2 * ghost;
-        // local start
-        l_s[0] = ghost;
-        l_s[1] = ghost;
-        l_s[2] = ghost;
-        // local end
-        l_e[0] = l_s[0] + l_l[0];
-        l_e[1] = l_s[1] + l_l[1];
-        l_e[2] = l_s[2] + l_l[2];
-
-        int x_his[comm.rx];
-        int y_his[comm.ry];
-        int z_his[comm.rz];
-        auto block_lo = [](int gl, int r, int pcoord) {
-            const int base = gl / r, rem = gl % r;
-            return pcoord * base + (pcoord < rem ? pcoord : rem);
-        };
-        x_lo = block_lo(glx, comm.rx, comm.px);
-        x_hi = x_lo + l_l[0] - 1;
-        y_lo = block_lo(gly, comm.ry, comm.py);
-        y_hi = y_lo + l_l[1] - 1;
-        z_lo = block_lo(glz, comm.rz, comm.pz);
-        z_hi = z_lo + l_l[2] - 1;
+        cs2 = 1.0 / 3.0;
     };
+
+    void setup_Cartesian();
+    void setup_Local();
+    void setup_MPI();
+    void setup_f();
 
     void Initialize();
     void Collision();
-    void setup_subdomain();
-    void pack();
-    void exchange();
-    void unpack();
     void Streaming();
     void Boundary();
     void Update();
+    void Update1();
     void MPIoutput(int n);
     void Output(int n);
+
+    void pack_f(buffer_f ff);
+    void unpack_f(buffer_f ff);
+
+    void exchange_f();
+
+    void passf(buffer_f ff);
 };
 #endif
